df81deeb39859ddb2b0f8819dcaa87e31b2b9361
Paul Holden <paul@holden.dev>
2023-03-28 09:00:38 +0100

    Add B2Draw.

---
+++ CommonB2Draw.go
@@ -0,0 +1,81 @@
+package box2d
+
+// Color for debug drawing. Each value has the range [0,1].
+type B2Color struct {
+	r, g, b, a float32
+}
+
+func MakeB2Color() B2Color {
+	return MakeB2ColorRGB(0, 0, 0)
+}
+
+func MakeB2ColorRGB(r, g, b float32) B2Color {
+	return MakeB2ColorRGBA(0, 0, 0, 1)
+}
+
+func MakeB2ColorRGBA(r, g, b, a float32) B2Color {
+	return B2Color{r, g, b, a}
+}
+
+func (c *B2Color) SetRGB(r, g, b float32) {
+	c.SetRGBA(r, g, b, 1)
+}
+
+func (c *B2Color) SetRGBA(r, g, b, a float32) {
+	c.r = r
+	c.g = g
+	c.b = b
+	c.a = a
+}
+
+var B2Draw_Flags = struct {
+	E_shapeBit        uint32 //< draw shapes
+	E_jointBit        uint32 //< draw joint connections
+	E_aabbBit         uint32 //< draw axis aligned bounding boxes
+	E_pairBit         uint32 //< draw broad-phase pairs
+	E_centerOfMassBit uint32 //< draw center of mass frame
+}{
+	E_shapeBit:        0x0001,
+	E_jointBit:        0x0002,
+	E_aabbBit:         0x0004,
+	E_pairBit:         0x0008,
+	E_centerOfMassBit: 0x0010,
+}
+
+// Implement and register this class with a b2World to provide debug drawing of physics
+// entities in your game.
+type B2Draw interface {
+	// Set the drawing flags.
+	SetFlags(flags uint32)
+
+	// Get the drawing flags.
+	GetFlags() uint32
+
+	// Append flags to the current flags.
+	AppendFlags(flags uint32)
+
+	// Clear flags from the current flags.
+	ClearFlags(flags uint32)
+
+	// Draw a closed polygon provided in CCW order.
+	DrawPolygon(vertices []B2Vec2, vertexCount int32, color B2Color)
+
+	// Draw a solid closed polygon provided in CCW order.
+	DrawSolidPolygon(vertices []B2Vec2, vertexCount int32, color B2Color)
+
+	// Draw a circle.
+	DrawCircle(center B2Vec2, radius float32, color B2Color)
+
+	// Draw a solid circle.
+	DrawSolidCircle(center B2Vec2, radius float32, axis B2Vec2, color B2Color)
+
+	// Draw a line segment.
+	DrawSegment(p1 B2Vec2, p2 B2Vec2, color B2Color)
+
+	// Draw a transform. Choose your own length scale.
+	// @param xf a transform.
+	DrawTransform(xf B2Transform)
+
+	// Draw a point.
+	DrawPoint(p B2Vec2, size float32, color B2Color)
+}
